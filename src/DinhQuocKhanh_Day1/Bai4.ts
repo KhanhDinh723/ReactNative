2526faee8b7fcca5b4f3aad28b76591b922d9d60
Khanh <khanhacademic723@gmail.com>
2025-08-21 11:37:18 +0700

    Bai4Done

---
+++ src/DinhQuocKhanh_Day1/Bai4.ts
@@ -0,0 +1,18 @@
+// 4. Create a class Rectangle with width and height. Write a method to calculate area and perimeter.
+
+export class Rectangle {
+  constructor(public width: number, public height: number) {}
+  //method
+
+  display(): void {
+    const p = (this.height + this.width) * 2;
+    const a = this.width * this.height;
+
+    console.log(
+      `Width:${this.width}, Height:${this.height},"\n"S: ${a}, P:${p}`
+    );
+  }
+}
+
+const Bai4 = new Rectangle(5.7, 6.7);
+Bai4.display();
